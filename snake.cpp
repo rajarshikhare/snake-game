df13a3a5d014badcbd4dd62042660cc0fab1f396
rajarshikhare <rajarshi.vaibhav@gmail.com>
2017-03-18 13:47:25 +0530

    first start

---
+++ snake.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	int i;
+	char c;
+	cout<<"|";
+	while(1){
+		c = getch();
+		if(c == 's'){
+			system("cls");
+			cout<<endl<<"|";
+		}
+	}
+	
+	
+	
+}
