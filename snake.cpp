2d2108a34ddb06318d4b555389ec40531b01143c
rajarshikhare <rajarshi.vaibhav@gmail.com>
2017-03-20 22:11:39 +0530

    version 1

--- snake.cpp
+++ snake.cpp
@@ -1,19 +1,26 @@
 #include<iostream>
+#include "position.h"
 #include <conio.h>
 using namespace std;
 
+int lenght = 10;
+
+typedef struct body{
+	int x,y;
+} body ;
+
+
 int main(){
-	int i;
-	char c;
-	cout<<"|";
-	while(1){
-		c = getch();
-		if(c == 's'){
-			system("cls");
-			cout<<endl<<"|";
-		}
+	system("mode 132,31");
+	int i,j = 0,k;
+	body snake[10];
+	for(i = 1; i <= lenght; i = i + 1 ){
+		cout<<"@";
+		snake[10-i].x = i;
+		snake[10-j].y = 0;
+		
 	}
-	
-	
-	
+	char c;
 }
+
+
