c2c79c46c01f0a9b5004fb328c98d0c02894628f
rajarshikhare <rajarshi.vaibhav@gmail.com>
2017-03-22 11:30:33 +0530

    final version

--- snake.cpp
+++ snake.cpp
@@ -5,27 +5,29 @@ using namespace std;
 
 int lenght = 10;
 int speed = 90;
+int game_point = 0;
 
 typedef struct body{
 	int x,y;
 } body ;
 
-body snake[10];
+body snake[80];
 
 int food_x = 63,food_y = 24;
 
-void change_food_location();
+void food_encounter();
 void move(char);
+void check_game();
 
-void delay(int time){
+void delay(int speed){
 	int c,d;
-	for ( c = 1 ; c <= time*time ; c++ )
-       for ( d = 1 ; d <= time*time ; d++ )
+	for ( c = 1 ; c <= speed*speed ; c++ )
+       for ( d = 1 ; d <= speed*speed ; d++ )
        {}
 }
 
 int main(){
-	system("mode 132,31");
+	system("mode 80,31");
 	int i,j = 0,k;
 	
 	for(i = 1; i <= lenght; i = i + 1 ){
@@ -50,27 +52,28 @@ int main(){
 void move(char c){
 	int i,j,k;
 	gotoxy(food_x,food_y);
-	cout<<"#";
+	cout<<"@";
 	if(c == 'd'){
-			gotoxy(snake[9].x, snake[9].y);
+			gotoxy(snake[lenght - 1].x, snake[lenght - 1].y);
 			cout<<" ";
 			for(i = lenght - 1; i > 0; i--){
 				snake[i].x = snake[i-1].x;
 				snake[i].y = snake[i-1].y;
 			}
-			if(snake[0].x/132){
+			if(snake[0].x/80){
 				snake[0].x = 0;
 			} else {
 				snake[0].x = snake[0].x + 2;
 			}
 			gotoxy(snake[0].x, snake[0].y);
+			check_game();
 			if((snake[0].x == food_x || snake[0].x == food_x + 1) && snake[0].y == food_y){
 				lenght++;
-				change_food_location();
+				food_encounter();
 			}
 			cout<<"*";
 		} else if(c == 's'){
-			gotoxy(snake[9].x, snake[9].y);
+			gotoxy(snake[lenght - 1].x, snake[lenght - 1].y);
 			cout<<" ";
 			for(i = lenght - 1; i > 0; i--){
 				snake[i].x = snake[i-1].x;
@@ -82,33 +85,33 @@ void move(char c){
 				snake[0].y++;
 			}
 			gotoxy(snake[0].x, snake[0].y);
+			check_game();
 			if((snake[0].x == food_x || snake[0].x == food_x + 1) && snake[0].y == food_y){
 				lenght++;
-				change_food_location();
-				speed++;
+				food_encounter();
 			}
 			cout<<"*";
 		} else if(c == 'a'){
-			gotoxy(snake[9].x, snake[9].y);
+			gotoxy(snake[lenght - 1].x, snake[lenght - 1].y);
 			cout<<" ";
 			for(i = lenght - 1; i > 0; i--){
 				snake[i].x = snake[i-1].x;
 				snake[i].y = snake[i-1].y;
 			}
 			if(snake[0].x <= 0){
-				snake[0].x = 132;
+				snake[0].x = 80;
 			} else {
 				snake[0].x = snake[0].x - 2;
 			}
 			gotoxy(snake[0].x, snake[0].y);
+			check_game();
 			if((snake[0].x == food_x || snake[0].x == food_x + 1) && snake[0].y == food_y){
 				lenght++;
-				change_food_location();
-				speed++;
+				food_encounter();
 			}
 			cout<<"*";
 		} else if(c == 'w'){
-			gotoxy(snake[9].x, snake[9].y);
+			gotoxy(snake[lenght - 1].x, snake[lenght - 1].y);
 			cout<<" ";
 			for(i = lenght - 1; i > 0; i--){
 				snake[i].x = snake[i-1].x;
@@ -120,20 +123,36 @@ void move(char c){
 				snake[0].y--;
 			}
 			gotoxy(snake[0].x, snake[0].y);
+			check_game();
 			if((snake[0].x == food_x || snake[0].x == food_x + 1) && snake[0].y == food_y){
 				lenght++;
-				change_food_location();
-				speed++;
+				food_encounter();
 			}
 			cout<<"*";
 		}
+		gotoxy(0,30);
+		cout<<"Game point : "<<game_point;
 }
 
-void change_food_location(){
+void food_encounter(){
 	gotoxy(food_x,food_y);
 	cout<<" ";
 	food_x = rand() % 130;
 	food_y = rand() % 30;
+	speed = speed--;
+	game_point+=5;
+}
+
+void check_game(){
+	int i;
+	for(i = 1; i < lenght; i++){
+		if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+			gotoxy(80/2-5,31/2);
+			cout<<"Game over and your score was : "<<game_point<<endl;
+			getch();
+			exit(0);
+		}
+	}
 }
 
 
